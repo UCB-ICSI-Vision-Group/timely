8f74b000e8ea42ad8a4047c3c598477fea0486a9
Sergey Karayev <sergeykarayev@gmail.com>
2012-01-26 18:30:55 -0800

    sped up bounding box stuff

--- synthetic/test/bounding_box.py
+++ synthetic/test/bounding_box.py
@@ -1,4 +1,4 @@
-import numpy as np
+from synthetic.common_imports import *
 
 from synthetic.bounding_box import BoundingBox
 
@@ -6,6 +6,24 @@ def test_convert_to_and_fro():
   bb = np.array([ 139.,  200.,   69.,  102.])
   bb_c = BoundingBox.convert_arr_to_corners(bb)
   bb2 = BoundingBox.convert_arr_from_corners(bb_c)
+  assert(np.all(bb == bb2))
+
+def test_convert_to_and_fro_with_array():
+  bb1 =    np.array([ 139.,  200.,   69.,  102.])
+  bb2 =    np.array([ 139.,  200.,   69.,  51.])
+  bb3 =    np.array([ 239.,  300.,   69.,  51.])
+  bb = np.vstack((bb1,bb2,bb3))
+  bb = np.tile(bb, (100000,1))
+  print(bb.shape)
+  tt = ut.Timer()
+  tt.tic()
+  bb_c = BoundingBox.convert_arr_to_corners(bb)
+  tt.toc()
+  print(bb_c.shape)
+  tt.tic()
+  bb2 = BoundingBox.convert_arr_from_corners(bb_c)
+  tt.toc()
+  print(bb2.shape)
   assert(np.all(bb == bb2))
 
 def test_get_overlap():
@@ -36,7 +54,15 @@ def test_get_overlap_with_array():
   bb2 =    np.array([ 139.,  200.,   69.,  51.])
   bb3 =    np.array([ 239.,  300.,   69.,  51.])
   bb = np.vstack((bb1,bb2,bb3))
+  numtimes = 100000
+  bb = np.tile(bb, (numtimes,1))
+  tt = ut.Timer()
+  tt.tic()
   ov = BoundingBox.get_overlap(bb,bbgt)
+  tt.toc()
+  tt.tic()
+  ov = BoundingBox.get_overlap(bb,bbgt)
+  tt.toc()
   print(ov)
-  assert(np.all(ov == np.array([1,0.5,0])))
+  assert(np.all(ov == np.tile(np.array([1,0.5,0]),numtimes)))
 
