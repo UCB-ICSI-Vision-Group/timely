7585d95051611562ac3c427b1fbca7aaa9de9c0c
Tobias Baumgartner <tobi.baum@googlemail.com>
2012-02-21 02:01:30 -0800

    also linear!

--- synthetic/config.py
+++ synthetic/config.py
@@ -192,8 +192,8 @@ def get_classifier_dirname(classifier):
   makedirs(dirname)
   return dirname
 
-def get_classifier_svm_name(cls, C, gamma, current_fold):
-  dirname = join(res_dir, 'classify_svm')
+def get_classifier_svm_name(cls, C, gamma, current_fold, kernel):
+  dirname = join(res_dir, kernel, 'classify_svm')
   makedirs(dirname) 
   if current_fold == -1: 
     filename = join(dirname, '%s_%f_%f'%(cls, C, gamma))
