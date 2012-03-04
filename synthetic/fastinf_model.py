a34ad7bf6c3383ef327212a540f283aaa33b22b6
Sergey Karayev <sergeykarayev@gmail.com>
2012-03-04 13:38:35 -0800

    fuck it, not caching

--- synthetic/fastinf_model.py
+++ synthetic/fastinf_model.py
@@ -30,7 +30,8 @@ class FastinfModel(InferenceModel):
     self.num_actions = num_actions
     self.tt = ut.TicToc().tic()
     self.process = pexpect.spawn(self.cmd)
-    self.reset()
+    marginals = self.get_marginals()
+    #print("FastinfModel: Computed initial marginals in %.3f sec"%self.tt.qtoc())
 
   def save_cache(self):
     "Write cache out to file with cPickle."
@@ -68,7 +69,6 @@ class FastinfModel(InferenceModel):
     Is actually instantaneous due to caching.
     """
     observations = taken = np.zeros(self.num_actions)
-    self.steps = 0
     self.update_with_observations(taken,observations)
 
   def get_marginals(self,evidence=None):
@@ -83,14 +83,13 @@ class FastinfModel(InferenceModel):
         marginals = self.cache[evidence]
         self.p_c = np.array([m[1] for m in marginals[:20]])
         return marginals
-      self.process.sendline(evidence)
-      self.steps += 1
+      else:
+        self.process.sendline(evidence)
     self.process.expect('Enter your evidence')
     output = self.process.before
     marginals = FastinfModel.extract_marginals(output)
-    # TODO: not caching for fear of ulimit
-    if self.steps < 3:
-      self.cache[evidence] = marginals
+    # DOn't cache anything!
+    #self.cache[evidence] = marginals
     self.p_c = np.array([m[1] for m in marginals[:20]])
     return marginals
 
