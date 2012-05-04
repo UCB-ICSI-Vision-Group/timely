bd02d33053338e1bb856cb1815e4ac3408bd9a4f
Sergey Karayev <sergeykarayev@gmail.com>
2012-05-03 23:43:15 -0700

    beuatiful

--- synthetic/temp_viz2.py
+++ synthetic/temp_viz2.py
@@ -21,9 +21,12 @@ def drawmatrix_channels(df, x_tick_rot=90,
 
     fig = plt.figure()
 
-    if size is not None:
-        fig.set_figwidth(size[0])
-        fig.set_figheight(size[1])
+    if size:
+      fig.set_figwidth(size[0])
+      fig.set_figheight(size[1])
+    else:
+      fig.set_figwidth(max(6,m.shape[1]/1))
+      fig.set_figheight(max(6,m.shape[0]/1))
 
     ax_im = fig.add_subplot(1, 1, 1)
 
@@ -53,61 +56,74 @@ def drawmatrix_channels(df, x_tick_rot=90,
 
     #Formatting:
     ax = ax_im
-    ax.grid(True)
-
-    #ax.set_axis_off()
     ax.set_xticks(np.arange(N+1))
-    ax.set_xticklabels(channel_names,rotation=x_tick_rot)
-    fig.autofmt_xdate(rotation=x_tick_rot)
+    ax.set_xticklabels(channel_names)
     for tick in ax.xaxis.iter_ticks():
       tick[0].label2On = True
       tick[0].label1On = False
       tick[0].label2.set_rotation(x_tick_rot)
+      tick[0].label2.set_fontsize('x-large')
 
     ax.set_yticks(np.arange(N))
-    ax.set_yticklabels(channel_names)
+    ax.set_yticklabels(channel_names,size='x-large')
+
+    ax.yaxis.set_minor_locator(ticker.FixedLocator(np.arange(-.5,N+0.5)))
+    ax.xaxis.set_minor_locator(ticker.FixedLocator(np.arange(-.5,N+1.5)))
+    ax.grid(False,which='major')
+    ax.grid(True,which='minor',ls='-',lw=3,c='w')
+
+    #Make the tick-marks invisible:
+    for line in ax.xaxis.get_ticklines() + ax.yaxis.get_ticklines():
+        line.set_markeredgewidth(0)
+    for line in ax.xaxis.get_minorticklines()+ax.yaxis.get_minorticklines():
+        line.set_markeredgewidth(0)
+    if title is not None:
+        ax.set_title(title)
 
     ax.set_ybound([-0.5, N - 0.5])
     ax.set_xbound([-0.5, N + 0.5])
-
-    #Make the tick-marks invisible:
-    for line in ax.xaxis.get_ticklines():
-        line.set_markeredgewidth(0)
-
-    for line in ax.yaxis.get_ticklines():
-        line.set_markeredgewidth(0)
-
-    if title is not None:
-        ax.set_title(title)
 
     #The following produces the colorbar and sets the ticks
     #Set the ticks - if 0 is in the interval of values, set that, as well
     #as the maximal and minimal values:
     if min_val < 0:
         ticks = [color_min, min_val, 0, max_val, color_max]
-    #Otherwise - only set the minimal and maximal value:
+    #Otherwise - only set the maximal value:
     else:
-        ticks = [color_min, min_val, max_val, color_max]
-    ticks = np.unique(ticks)
+        ticks = [color_min, max_val, color_max]
 
     # line separating prior
-    l = mpl.lines.Line2D([N-0.48,N-0.48],[-.48,N-0.48],ls='--',c='gray')
+    l = mpl.lines.Line2D([N-0.5,N-0.5],[-.5,N-0.5],
+      ls='--',c='gray',lw=2)
     l = ax.add_line(l)
+    l.set_zorder(3)
+
+    for i in xrange(0, m.shape[0]):
+      for j in xrange(0,m.shape[1]):
+        val = m.as_matrix()[i,j]
+        if not np.isnan(val):
+          ax.text(j-0.2,i+0.1,'%.2f'%val)
+
+    # Just doing ax.set_frame_on(False) results in weird thin lines
+    # from imshow() on the edges. This covers them up.
+    for spine in ax.spines.values():
+        spine.set_edgecolor('w')
 
     #This makes the colorbar:
     cb = fig.colorbar(im, cax=ax_cb, orientation='horizontal',
                       cmap=cmap,
-                      norm=im.norm,
-                      boundaries=np.linspace(color_min, color_max, 256),
                       ticks=ticks,
                       format='%.2f')
+    cb.ax.artists.remove(cb.outline)
     return fig
 
 df = DataFrame.load('temp.df')
 cmap = plt.cm.gray_r
 f = drawmatrix_channels(df,cmap=cmap)
+f.savefig('temp_synth.png')
 
-cmap = plt.cm.jet
+cmap = plt.cm.Reds
+color_anchor=[0,0.5]
 df = DataFrame.load('temp2.df')
-f2 = drawmatrix_channels(df,cmap=cmap)
-f3 = drawmatrix_channels(df,size=(10,10),cmap=cmap)
+f3 = drawmatrix_channels(df,cmap=cmap,color_anchor=color_anchor)
+f3.savefig('temp_pascal.png')
