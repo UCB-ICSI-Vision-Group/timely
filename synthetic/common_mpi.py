c2c21ea70f4d407c36e4e64a5d51101c17a6bcc5
Sergey Karayev <sergeykarayev@gmail.com>
2012-05-29 16:16:43 -0700

    oops, i think my safebarrier was borked

--- synthetic/common_mpi.py
+++ synthetic/common_mpi.py
@@ -15,14 +15,14 @@ def safebarrier(comm, tag=0, sleep=0.01):
   size = comm.Get_size()
   if size == 1:
     return
-    rank = comm.Get_rank()
-    mask = 1
-    while mask < size:
-      dst = (rank + mask) % size
-      src = (rank - mask + size) % size
-      req = comm.isend(None, dst, tag)
-      while not comm.Iprobe(src, tag):
-        time.sleep(sleep)
-        comm.recv(None, src, tag)
-        req.Wait()
-        mask <<= 1
+  rank = comm.Get_rank()
+  mask = 1
+  while mask < size:
+    dst = (rank + mask) % size
+    src = (rank - mask + size) % size
+    req = comm.isend(None, dst, tag)
+    while not comm.Iprobe(src, tag):
+      time.sleep(sleep)
+    comm.recv(None, src, tag)
+    req.Wait()
+    mask <<= 1
